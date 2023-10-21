d0d7842206b8d18ee374acea6cdb94ce24ddfcdb
mark-feodosiy <mark-feodosiy@yandex.ru>
2023-10-21 09:58:00 +0300

    2-я домашка с циклами

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
             int depositTotal = 0;
             int month = 0;
             while (depositTotal <= 2459000) {
-                depositTotal = depositTotal + 15000;
+                depositTotal = depositTotal + 15000 + depositTotal / 100;
                 month = month + 1;
                     System.out.println("Месяц " + month + ", сумма накоплений равна " + depositTotal + " рублей");
             }
