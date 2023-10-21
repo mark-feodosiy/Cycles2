4ff5816ab57ae91d63d764a540759918f4ee078e
mark-feodosiy <mark-feodosiy@yandex.ru>
2023-10-21 07:16:12 +0300

    2-я домашка с циклами

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println();
+        int days = 10; // изначальное количество дней в Париже
+        if (days == 0) {
+            System.out.println("Желаем приятной поездки!");
+        } else {
+            while (days > 0) {
+                System.out.println("До конца поездки осталось " + days + " дней.");
+                days--;
+            }
+            System.out.println("Путешествие окончено. Пора возвращаться домой.");
+        }
     }
+
 }
